a74c90c7dc7a66009be0f7f023e2a1007a4a808e
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-08-26 08:00:42 +0530

    "Updated Button, CodeBlocks, HighlightText, and Home components with minor styling and formatting changes."

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import {BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -10,5 +11,6 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    
   </React.StrictMode>
 );
