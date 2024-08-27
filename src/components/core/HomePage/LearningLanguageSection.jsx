75a0979b3f897f47304f885ffb3d1562654e010d
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-08-27 17:06:44 +0530

    "Updated LearningLanguageSection and Home components with new elements and layout changes"

--- src/components/core/HomePage/LearningLanguageSection.jsx
+++ src/components/core/HomePage/LearningLanguageSection.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import HighlightText from './HighlightText'
+import CTAButton from './Button'
 import know_your_progress from "../../../assets/Images/Know_your_progress.png"
 import compare_with_others from "../../../assets/Images/Compare_with_others.png"
 import plan_your_lesson from "../../../assets/Images/Plan_your_lessons.png"
 
 const LearningLanguageSection =()=>{
     return (
-        <div className='mt-[130px]'>
-            <div className='flex flex-col gap-5'>
+        <div className='mt-[130px] mb-32'>
+            <div className='flex flex-col gap-5 items-center'>
                 <div className='text-4xl font-semibold text-center'>
-                
+
                     Your Swiss Knife for
                     <HighlightText text={" learning any language"}/>
                 </div>
@@ -34,6 +35,13 @@ const LearningLanguageSection =()=>{
                         alt='plan your lesson'
                         className='object-contain -ml-36'
                     />
+                </div>
+                <div className='w-fit'>
+                    <CTAButton active={true} linkto={"/signup"}>
+                        <div>
+                            Learn More
+                        </div>
+                    </CTAButton>
                 </div>
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,10 @@ import Banner from "../assets/Images/banner.mp4"
 import CodeBlocks from "../components/core/HomePage/CodeBlocks"
 import TimelineSection from "../components/core/HomePage/TimelineSection"
 import LearningLanguageSection from "../components/core/HomePage/LearningLanguageSection"
+import InstructorSection from '../components/core/HomePage/InstructorSection'
+import Footer from "../components/common/Footer"
 
-
+import ExploreMore from '../components/core/HomePage/ExploreMore'
 
 const Home = () => {
   return (
@@ -127,6 +129,8 @@ const Home = () => {
             />
         </div>
 
+        <ExploreMore/>
+
       </div>
 
       {/*Section 2  */}
@@ -184,8 +188,17 @@ const Home = () => {
 
       {/*Section 3 */}
 
+      <div className='w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between
+      gap-8 bg-richblack-900 text-white'>
+
+        <InstructorSection/>
+        <h2 className='text-center text-4xl font-semibold mt-10'>Review from Other Learners</h2>
+
+      </div>
+
 
       {/*Footer */}
+      <Footer/>
 
 
     </div>
