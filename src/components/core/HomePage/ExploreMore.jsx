2ed6c0b72f1de84c808432f04bd44eed8d0c1b98
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-08-27 17:35:58 +0530

    "Added HighlightText component and updated ExploreMore component with new UI elements and styling"

--- src/components/core/HomePage/ExploreMore.jsx
+++ src/components/core/HomePage/ExploreMore.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {HomePageExplore} from "../../../data/homepage-explore"
+import HighlightText from './HighlightText';
 
 const tabsName =[
     "Free",
@@ -21,7 +22,34 @@ const ExploreMore = () => {
         setCurrentCard(result[0].courses[0].heading);
     }
   return (
-    <div>ExploreMore</div>
+    <div>
+        <div className='text-4xl font-semibold text-center'>
+            Unlock the
+            <HighlightText text={"Power of Code"}/>
+        </div>
+        <p className='text-center text-richblack-300 text-sm text-[16px] mt-3'>
+            Learn to build anything you can imagine
+        </p>
+
+        <div className='mt-5 flex flex-row rounded-full bg-richblack-800 mb-5 border-richblack-100
+        px-1 py-1'>
+            {
+                tabsName.map((element,index)=>{
+                    return (
+                        <div className={`text-[16px] flex flex-row items-center gap-2
+                        ${currentTab === element
+                        ?"bg-richblack-900 text-richblack-5 font-medium"
+                        :"text-richblack-200"} rounded-full transition-all duration-200 cursor-pointer
+                        hover:bg-richblack-900 hover:text-richblack-5 px-7 py-2`}
+                        key={index}
+                        onClick={()=>setMyCards(element)}>
+                            {element}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </div>
   )
 }
 
