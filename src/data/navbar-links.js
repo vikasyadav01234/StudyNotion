a725bf0c0a37403202cddd1a7bd7ab526dd05add
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-08-30 08:42:05 +0530

    "Refactored CodeBlocks component and updated NavbarLinks data"

--- src/data/navbar-links.js
+++ src/data/navbar-links.js
@@ -1,4 +1,4 @@
-export const NavbarLinks = [
+const NavbarLinks = [
   {
     title: "Home",
     path: "/",
@@ -16,3 +16,4 @@ export const NavbarLinks = [
     path: "/contact",
   },
 ];
+export default NavbarLinks;
