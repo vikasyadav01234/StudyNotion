2d80659cad49721d986f18f1b2a79dc48339ce85
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-09-21 15:25:11 +0530

    "Updated various files in the src directory, including CSS, JSX, and JS files, with changes to code structure, syntax, and formatting."

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,7 +1,7 @@
 export const formattedDate = (date) => {
-    return new Date (date).toLocaleDateString("en-US",{
-        year: "numeric",
-        month: "long",
-        day: "numeric"
+    return new Date(date).toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
     })
-}
+  }
