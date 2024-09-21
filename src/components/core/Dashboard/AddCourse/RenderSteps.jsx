29e486c983ac2e2bd55e609d4a935de76bda00fc
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-09-21 15:00:46 +0530

    "Added AddCourse component and updated IconBtn, CourseBuilderForm, and RenderSteps components"

--- src/components/core/Dashboard/AddCourse/RenderSteps.jsx
+++ src/components/core/Dashboard/AddCourse/RenderSteps.jsx
@@ -1,60 +1,63 @@
 import React from 'react'
-import {FaCheck} from 'react-icons/fa'
+import { FaCheck } from 'react-icons/fa';
 import { useSelector } from 'react-redux'
-import CourseBuilderForm from './CourseBuilder/CourseBuilderForm'
-import CourseInformationForm from './CourseInformation/CourseInformationForm'
+import CourseBuilderForm from './CourseBuilder/CourseBuilderForm';
+import CourseInformationForm from './CourseBuilder/CourseBuilderForm';
 
-const RenderSteps =()=>{
-    const {step} = useSelector((state) => state.course);
+const RenderSteps = () => {
 
-    const steps=[
+    const {step} = useSelector((state)=> state.course);
+
+    const steps = [
         {
             id:1,
-            title:"Course Information",
+            title: "Course Information",
         },
         {
             id:2,
-            title:"Course Builder",
+            title: "Course Builder",
         },
         {
             id:3,
-            title:"Publish",
+            title: "Publish",
         },
     ]
 
-    return (
-        <>
-            <div>
-                {
-                    steps.map((item)=>(
-                        <>
-                            <div>
-                                <div>
-                                    {
-                                        step> item.id?(<FaCheck/>):(item.id)
-                                    }
-                                </div>
-                            </div>
-                        </>
-                    ))
-                }
-            </div>
-            <div>
-                {
-                    steps.map((item)=>(
-                        <>
-                            <div>
-                                <p>{item.title}</p>
-                            </div>
-                        </>
-                    ))
-                }
-            </div>
-            {step === 1 && <CourseInformationForm/>}
-            {step === 2 && <CourseBuilderForm/>}
-            {/* {step === 3 && <PublishCourse/>} */}
-        </>
-    )
+  return (
+    <>
+        <div>
+            {steps.map( (item) => (
+                <>
+                    <div>
+                        <div className={`${step === item.id 
+                        ? "bg-yellow-900 border-yellow-50 text-yellow-50" 
+                        : "border-richblack-700 bg-richblack-800 text-richblack-300"}`}>
+
+                        {
+                            step > item.id ? (<FaCheck/>) :(item.id)
+                        }
+
+                        </div>
+                    </div>
+                   {/* Add COde for dashes between the labels */}
+                </>
+            ) )}
+        </div>
+        <div>
+            {steps.map((item) => (
+                <>
+                    <div>
+                        <p>{item.title}</p>
+                    </div>
+                </>
+            ))}
+        </div>
+
+        {step === 1 && <CourseInformationForm />}
+        {step === 2 && <CourseBuilderForm/>}
+        {/* {step===3 && <PublishCourse/>} */}
+    </>
+  )
 }
 
 export default RenderSteps
