a6d2df8ffe636488581ca74e9caa3f8c450bc893
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-09-21 17:57:34 +0530

    "Added imports, state variables, and functionality to CourseBuilderForm component"

--- src/components/core/Dashboard/AddCourse/CourseBuilderForm/CourseBuilderForm.jsx
+++ src/components/core/Dashboard/AddCourse/CourseBuilderForm/CourseBuilderForm.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import {useForm} from 'react-hook-form'
+import IconBtn from '../../../../common/IconBtn';
+import {MdAddCircleOutline} from "react-icons/bi"
+import { BiAddToQueue } from 'react-icons/bi';
+import { useDispatch,useSelector } from 'react-redux';
+import { BiRightArrow } from 'react-icons/bi';
+import {setCourse,setEditCourse,setStep} from '../../../../../slices.courseSlice';
+import {toast} from 'react-hot-toast'
+import {createSection,updateSection} from '../../../../../services/operations/courseDetailsAPI'
 
 const CourseBuilderForm = () => {
+
+  const {register,handleSubmit,setValue, formState:{errors}}=useForm();
+  const [editSectionName,setEditSectionName] = useState(null);
+  const {course}=useSelector((state)=>state.course);
+  const dispatch=useDispatch();
+  const {token} = useSelector((state)=>state.auth);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(()=>{
+    console.log("Updated");
+  },[course])
+
+  const onSubmit = async(data) =>{
+    setLoading(true);
+    let result;
+  }
+
   return (
     <div className='text-white'>
       Step 2 me aapka swagat h 
