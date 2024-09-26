f6b8c9073ec9c7f41da7c06066aed7833b39164d
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-09-26 16:45:47 +0530

    "Updated controllers for Category, Course, Section, and Subsection with new functionality and error handling; added auth middleware"

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,10 +5,13 @@ const User = require("../models/User");
 //auth
 exports.auth = async (req, res, next) => {
     try{
+
+        console.log("BEFORE ToKEN EXTRACTION");
         //extract token
         const token = req.cookies.token 
                         || req.body.token 
-                        || req.header("Authorisation").replace("Bearer ", "");
+                        || req.header("Authorization").replace("Bearer ", "");
+        console.log("AFTER ToKEN EXTRACTION");
 
         //if token missing, then return response
         if(!token) {
